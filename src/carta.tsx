bdd244b715d77be47faf65e56d5cbf891e20d6c7
Douglas.Mariano <douglas.mariano@estudante.ufjf.br>
2025-11-06 01:37:16 -0300

    cria carta

---
+++ src/carta.tsx
@@ -0,0 +1,23 @@
+import "./arte.css";
+
+export type CardProps = {
+  id: number;
+  value: string;
+  nome: string;
+  virada: boolean;
+};
+
+export function Card({ value, nome, virada }: CardProps) {
+  return (
+    <div className={`card ${virada ? "flipped" : ""}`}>
+      <div className="card-inner">
+        <div className="card-front">❓</div>
+        <div className="card-back">
+          <span className="emoji">{value}</span>
+          <span className="nome">{nome}</span>
+        </div>
+      </div>
+    </div>
+  );
+}
+
